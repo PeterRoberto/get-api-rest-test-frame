aa75d738b2b249dc1fc4c6e9ae1c8ea968ea0eb6
Peter Roberto - Casa <peterrobertoweb@gmail.com>
2019-10-03 00:59:41 -0300

    Aprendendo Angular para teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatTableModule} from '@angular/material/table';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContatosListagemComponent } from './contatos-listagem/contatos-listagem.component';
+import { ContatoService } from './contato.service';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContatosListagemComponent 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatTableModule,
+    MatGridListModule
   ],
-  providers: [],
+  providers: [ ContatoService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
